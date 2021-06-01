b5cc217fa7e78c168ade000c3225b22da7f76247
Raphael Khan <raphaelkhan8@gmail.com>
2021-06-01 17:14:15 -0500

    added key prop to get rid of unique key list error

--- client/src/views/HomeView.js
+++ client/src/views/HomeView.js
@@ -9,8 +9,8 @@ const HomeView = () => {
       <h1>Latest Products</h1>
       <Row>
         {products.map(product => (
-          <Col sm={12} md={6} lg={4} xl={3}>
-            <Product key={product.id} product={product} />
+          <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
           </Col>
         ))}
       </Row>
