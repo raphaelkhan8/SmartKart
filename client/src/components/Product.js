51ec1477976fd19048a7cc2a392bb79f6f27e1aa
Raphael Khan <raphaelkhan8@gmail.com>
2021-06-01 17:33:16 -0500

    added Rating star component to Product card

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap';
+import Rating from './Rating';
 
 const Product = ({ product }) => {
   const { id, name, image, description, brand, price, countInStock, rating, numReviews } = product;
@@ -14,9 +15,9 @@ const Product = ({ product }) => {
         <Card.Title as='div'><strong>{name}</strong></Card.Title>
       </a>
 
-      <Card.Text as='div'>{description}</Card.Text>
-
-      <Card.Text as='div'><div className='my-3'>{rating}/5 from {numReviews} reviews</div></Card.Text>
+      <Card.Text as='div'>
+        <Rating rating={rating} numReviews={numReviews} />
+      </Card.Text>
       </Card.Body>
 
       <Card.Text as ='h3'>${price}</Card.Text>
