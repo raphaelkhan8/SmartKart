b59e076e1435576183e21901b04b97def69847c4
Raphael Khan <raphaelkhan8@gmail.com>
2021-07-04 18:32:55 -0500

    corrected incorrect variable name

--- client/src/actions/cartActions.js
+++ client/src/actions/cartActions.js
@@ -17,5 +17,5 @@ export const addToCart = (id, quantity) =>
       },
     })
 
-  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+  localStorage.setItem('cartItems', JSON.stringify(getState().userCart.cartItems))
 }
