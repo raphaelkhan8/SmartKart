c637aa09953abffbd55b250744fb465b774fab94
Raphael Khan <raphaelkhan8@gmail.com>
2021-07-04 16:53:09 -0500

    added quantity selector drop-down

--- client/src/views/ProductView.js
+++ client/src/views/ProductView.js
@@ -1,22 +1,34 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector  } from 'react-redux'
-import { Row, Col, Image, ListGroup, ListGroupItem, Card, Button } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, ListGroupItem, Card, Button, Form } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import { listProductDetails } from '../actions/productActions'
+// import { addToCart } from '../actions/cartActions'
 
-const ProductView = ({ match }) => {
+const ProductView = ({ match, history }) => {
+  const [quantity, setQuantity] = useState(0)
+
   const dispatch = useDispatch()
 
   const { loading, error, product } = useSelector(state => state.productDetails)
+  const { name, image, rating, numReviews, price, description, countInStock } = product
 
   useEffect(() => {
     dispatch(listProductDetails(match.params.id))
   }, [dispatch, match.params.id])
 
-  const { name, image, rating, numReviews, price, description, countInStock } = product
+  const addToCartHandler = () => {
+    history.push(`/cart/${match.params.id}?qty=${quantity}`)
+  }
+
+//   const addToCartHandler = () => {
+//     dispatch(addToCart(product._id, quantity))
+//     history.push('/cart')
+// }
+
 
   return (
     <div>
@@ -69,8 +81,26 @@ const ProductView = ({ match }) => {
                     </Row>
                   </ListGroupItem>
 
+                  {countInStock > 0 && (
+                    <ListGroupItem>
+                      <Row>
+                        <Col>Quantity</Col>
+                        <Col>
+                          <Form.Control as='select' value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+                            {[...Array(countInStock).keys()].map(num => (
+                              <option key={num + 1} value={num + 1}>
+                                {num + 1}
+                              </option>
+                            ))}
+                          </Form.Control>
+                        </Col>
+                      </Row>
+                    </ListGroupItem>
+                  )}
+
                   <ListGroupItem>
-                    <Button className='col-12' type='button' disabled={!countInStock}>Add To Cart</Button>
+                    <Button onClick={addToCartHandler} className='col-12' type='button' disabled={!countInStock}>
+                      Add To Cart</Button>
                   </ListGroupItem>
 
                 </ListGroup>
