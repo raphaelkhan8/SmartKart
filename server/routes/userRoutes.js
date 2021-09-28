12c3a9e6f1a71ebe65c4b0f5ec7853fdf18ca715
raphaelkhan8 <raphaelkhan8@gmail.com>
2021-09-27 21:48:30 -0500

    added update user profile routing

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { authUser, getUserProfile, createUser } = require('../controllers/userController')
+const { authUser, getUserProfile, updateUserProfile, createUser } = require('../controllers/userController')
 const protectRoutes = require('../middleware/authMiddleware')
 
 router.post('/', createUser)
 router.post('/login', authUser)
 router.get('/profile', protectRoutes, getUserProfile)
+router.put('/profile', protectRoutes, updateUserProfile)
 
 module.exports = router
