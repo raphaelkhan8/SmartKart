e9fdf855fb238120921462c7935a0e2773cf1941
Raphael Khan <raphaelkhan8@gmail.com>
2021-10-11 16:11:07 -0500

    removed redundant field

--- server/models/orderModel.js
+++ server/models/orderModel.js
@@ -44,11 +44,6 @@ const orderSchema = mongoose.Schema({
     required: true,
     default: 0.0
   },
-  taxPrice: {
-    type: Number, 
-    required: true,
-    default: 0.0
-  },
   totalPrice: {
     type: Number, 
     required: true,
