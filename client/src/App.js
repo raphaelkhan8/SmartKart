a986e92272df81c31c3cc329bd0e2ef7bc51cd14
Raphael Khan <raphaelkhan8@gmail.com>
2021-11-02 18:05:04 -0500

    created Order Details page and routing

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import ProfileView from './views/ProfileView'
 import ShippingView from './views/ShippingView'
 import PaymentView from './views/PaymentView'
 import PlaceOrderView from './views/PlaceOrderView'
+import OrderView from './views/OrderView'
 
 const App = () => {
   return (
@@ -27,6 +28,7 @@ const App = () => {
           <Route path='/shipping' component={ShippingView} />
           <Route path='/payment' component={PaymentView} />
           <Route path='/placeorder' component={PlaceOrderView} />
+          <Route path='/order/:id' component={OrderView} />
         </Container>
       </main>
       <Footer />
