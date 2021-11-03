ad209f42f7334c358733f658727720a22574c0b5
Raphael Khan <raphaelkhan8@gmail.com>
2021-11-03 13:08:02 -0500

    created function and server-side routing to update order to Paid

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -36,4 +36,29 @@ const getOrderById = asyncErrorHandler(async (req, res) => {
 })
 
 
-module.exports = { createOrder, getOrderById }
+const updateOrderToPaid = asyncErrorHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id)
+
+  if (order) {
+    order.isPaid = true
+    order.paidAt = Date.now()
+    // order.paymentResult values comes from Paypal
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address
+    }
+
+    const updateOrder = await order.save()
+
+    res.json(updateOrder)
+
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+})
+
+
+module.exports = { createOrder, getOrderById, updateOrderToPaid }
--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const protectRoutes = require('../middleware/authMiddleware')
-const { createOrder, getOrderById } = require('../controllers/orderController')
+const { createOrder, getOrderById, updateOrderToPaid } = require('../controllers/orderController')
 
 router.post('/', protectRoutes, createOrder)
 router.get('/:id', protectRoutes, getOrderById)
+router.put('/:id/pay', protectRoutes, updateOrderToPaid)
 
 module.exports = router
