80a029e758b4bfbdb40d721143afa1e5d7728bc0
raphaelkhan8 <raphaelkhan8@gmail.com>
2021-12-01 15:17:20 -0600

    removed unecessary dispatch from listUsers action

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -180,13 +180,6 @@ export const listUsers = () => async (dispatch, getState) => {
       payload: data
     })
 
-    dispatch({
-      type: USER_LOGIN_SUCCESS,
-      payload: data
-    })
-
-    localStorage.setItem('userInfo', JSON.stringify(data))
-
   } catch (error) {
     dispatch({
       type: USER_LIST_FAIL,
