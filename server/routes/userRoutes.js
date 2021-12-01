0ad32abbc489fa3cfc9ee024fd397d4475fe6a96
raphaelkhan8 <raphaelkhan8@gmail.com>
2021-12-01 16:21:13 -0600

    implemented deleting a user (server-side)

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { authUser, getUserProfile, updateUserProfile, createUser, getAllUsers } = require('../controllers/userController')
+const { authUser, getUserProfile, updateUserProfile, createUser, getAllUsers, deleteUser } = require('../controllers/userController')
 const { protectRoutes, isAdmin }  = require('../middleware/authMiddleware')
 
 router.post('/', createUser)
@@ -8,5 +8,6 @@ router.post('/login', authUser)
 router.get('/', protectRoutes, isAdmin, getAllUsers)
 router.get('/profile', protectRoutes, getUserProfile)
 router.put('/profile', protectRoutes, updateUserProfile)
+router.delete('/:id', protectRoutes, isAdmin, deleteUser)
 
 module.exports = router
