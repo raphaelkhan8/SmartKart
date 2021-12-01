e3590280ce70e5d657225093e1022c427f887533
raphaelkhan8 <raphaelkhan8@gmail.com>
2021-12-01 15:17:53 -0600

    added Admin-only navbar dropdown menu

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -37,6 +37,18 @@ const Header = () => {
             (<LinkContainer to='/login'>
               <Nav.Link><i className='fas fa-user'></i> SIGN IN</Nav.Link>
             </LinkContainer>)}
+            {userInfo && userInfo.isAdmin ? 
+            (<NavDropdown title='Admin' id='adminmenu'>
+              <LinkContainer to='/admin/userlist'>
+                <NavDropdown.Item>Users</NavDropdown.Item>
+              </LinkContainer>
+              <LinkContainer to='/admin/productlist'>
+                <NavDropdown.Item>Products</NavDropdown.Item>
+              </LinkContainer>
+              <LinkContainer to='/admin/orderlist'>
+                <NavDropdown.Item>Orders</NavDropdown.Item>
+              </LinkContainer>
+            </NavDropdown>) : <div></div>}
           </Nav>
           </Navbar.Collapse>
         </Container>
