3883c1a3032de1f61d3c880bda9d86d31a406145
raphaelkhan8 <raphaelkhan8@gmail.com>
2021-12-02 11:27:21 -0600

    server-side functionality/routing to get user by id/edit admin status

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
-const { authUser, getUserProfile, updateUserProfile, createUser, getAllUsers, deleteUser } = require('../controllers/userController')
+const { authUser, getUserProfile, updateUserProfile, createUser, getAllUsers, 
+    getUserById, updateAdminStatus, deleteUser } = require('../controllers/userController')
 const { protectRoutes, isAdmin }  = require('../middleware/authMiddleware')
 
 router.post('/', createUser)
@@ -8,6 +9,8 @@ router.post('/login', authUser)
 router.get('/', protectRoutes, isAdmin, getAllUsers)
 router.get('/profile', protectRoutes, getUserProfile)
 router.put('/profile', protectRoutes, updateUserProfile)
+router.get('/:id', protectRoutes, isAdmin, getUserById)
+router.put('/:id', protectRoutes, isAdmin, updateAdminStatus)
 router.delete('/:id', protectRoutes, isAdmin, deleteUser)
 
 module.exports = router
