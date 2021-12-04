95606976dbb86b7b0aab47e34591405f21ccddc0
raphaelkhan8 <raphaelkhan8@gmail.com>
2021-12-04 12:29:17 -0600

    implemented server-side functionality to delete a product

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { getProducts, getProductById } = require('../controllers/productController')
+const { getProducts, getProductById, deleteProduct } = require('../controllers/productController')
+const { protectRoutes, isAdmin } = require('../middleware/authMiddleware')
 
 
 router.get('/', getProducts)
-
 router.get('/:id', getProductById)
-
+router.delete('/:id', protectRoutes, isAdmin, deleteProduct)
 
 module.exports = router
